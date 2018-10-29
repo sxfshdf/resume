7a9cb0ad1d0908d56fa95df9e3021854450265e4
sixiaofeng <sxf754872493@gmail.com>
2018-10-29 12:46:12 +0800

    轮播调整

--- js/soomthly-navigation.js
+++ js/soomthly-navigation.js
@@ -1,46 +1,53 @@
 !function (){
   var view = document.querySelector('nav.menu')
-  let liTags = view.querySelectorAll('nav.menu > ul > li')
-  for( let i=0; i<liTags.length; i++){
-    liTags[i].onmouseenter = function(x){
-       x.currentTarget.classList.add('active')
+  var controller = {
+    view: null,
+    init: function(view){
+      this.view = view
+      let liTags = this.view.querySelectorAll('nav.menu > ul > li')
+      for( let i=0; i<liTags.length; i++){
+        liTags[i].onmouseenter = function(x){
+           x.currentTarget.classList.add('active')
+          }
+        liTags[i].onmouseleave = function(x){
+          x.currentTarget.classList.remove('active')
+        } 
       }
-    liTags[i].onmouseleave = function(x){
-      x.currentTarget.classList.remove('active')
-    } 
-  }
-  
-  function animate(time) {
-    requestAnimationFrame(animate);
-    TWEEN.update(time);
-  }
-  requestAnimationFrame(animate);
-  
-  let aTags = document.querySelectorAll('nav.menu > ul > li > a')
-  for(let i=0 ; i<aTags.length ; i++){
-    aTags[i].onclick = function(x){
-      x.preventDefault()
-      let a = x.currentTarget
-      let href = a.getAttribute('href')//不加getAttri的话会得到一个浏览器处理过的href 如：http://192.168.0.109:8080/#siteAbout
-  
-      let element = document.querySelector(href)
-      let top = element.offsetTop
-  
-      let currentTop = window.scrollY
-      let targetTop = top - 80
-      let s = targetTop - currentTop //路程
-      let t = Math.abs((s/100)*300) // 时间
-      if(t>500){t = 500} 
-  
-      var coords = { y: currentTop};// 起始位置
-      var tween = new TWEEN.Tween(coords) 
-      .to({ y: targetTop }, t) // 结束位置和时间
-      .easing(TWEEN.Easing.Quadratic.InOut)//缓动类型
-      .onUpdate(function() {
-        // coords.y 已经变了
-          window.scrollTo(0 , coords.y);//如何更新界面
-      })
-      .start();//开始缓动
+      
+      function animate(time) {
+        requestAnimationFrame(animate);
+        TWEEN.update(time);
+      }
+      requestAnimationFrame(animate);
+      
+      let aTags = this.view.querySelectorAll('nav.menu > ul > li > a')
+      for(let i=0 ; i<aTags.length ; i++){
+        aTags[i].onclick = function(x){
+          x.preventDefault()
+          let a = x.currentTarget
+          let href = a.getAttribute('href')//不加getAttri的话会得到一个浏览器处理过的href 如：http://192.168.0.109:8080/#siteAbout
+      
+          let element = document.querySelector(href)
+          let top = element.offsetTop
+      
+          let currentTop = window.scrollY
+          let targetTop = top - 80
+          let s = targetTop - currentTop //路程
+          let t = Math.abs((s/100)*300) // 时间
+          if(t>500){t = 500} 
+      
+          var coords = { y: currentTop};// 起始位置
+          var tween = new TWEEN.Tween(coords) 
+          .to({ y: targetTop }, t) // 结束位置和时间
+          .easing(TWEEN.Easing.Quadratic.InOut)//缓动类型
+          .onUpdate(function() {
+            // coords.y 已经变了
+              window.scrollTo(0 , coords.y);//如何更新界面
+          })
+          .start();//开始缓动
+        }
+      }
     }
   }
+  controller.init(view)
 }.call()
--- js/swiper.js
+++ js/swiper.js
@@ -3,7 +3,7 @@
   var controller = {
     view: null,
     swiper: null,
-    swiperOptions: {
+    swiperOptions:{
       loop: true,
       pagination: {
         el: '.swiper-pagination',
@@ -14,15 +14,10 @@
       }
     },
     init: function(view){
-      this.view = view
-      this.initSwiper()
-    },
-    initSwiper: function(){
-      this.swiper = new Swiper (
-        this.view.querySelector('.swiper-container'),
-        this.swiperOptions )
+      this.swiper = new Swiper ('.swiper-container', 
+      this.swiperOptions
+      )
     }
   }
   controller.init(view)
-  
 }.call()
